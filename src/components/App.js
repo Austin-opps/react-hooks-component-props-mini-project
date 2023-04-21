d5638a036c85fa6de1bf4b693503b3a741d4108f
Austin <akomen04@gmail.com>
2023-04-21 10:53:46 +0300

    Almost there

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header";
+import Header from "./header";
 import About from "./About";
-import ArticleList from "./ArticleList";
+import ArticleList from "./articleList";
 
 function App() {
   return (
